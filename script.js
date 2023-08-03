b035bc9701002dd1b2f7e958c0a9ca5f9890ce23
Xabier Damboriena <131868620+xabid98@users.noreply.github.com>
2023-08-03 08:05:52 +0200

    16x16 grid of square divs

---
+++ script.js
@@ -0,0 +1,15 @@
+const container=document.getElementById('container')
+
+
+let div = document.createElement("div");
+div.classList.add('card')
+
+for (let j = 0; j < 16; j++) {    
+    for (let i = 0; i < 16; i++) {
+        let m=480/16
+        let div = document.createElement("div");
+        div.classList.add('card')   
+        div.style.width=`${m}px`
+        container.appendChild(div)    
+    }
+}
